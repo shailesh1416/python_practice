9422b05c371355fb416b6621b0f3a92ea80d04aa
Shailesh Yadav <shaileshyadav1416@gmail.com>
2022-12-29 16:29:19 +0530

    first commit

---
+++ print_exercise.py
@@ -0,0 +1,7 @@
+print("Hello My name is Shailsh I live in boisar.")
+
+# modify code to return following output
+# ================output================
+# Hello
+# My name is Shailsh
+# I live in boisar.
