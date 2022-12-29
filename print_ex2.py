fb092525a9ff2c19bf93455d9d278c9090389f69
Shailesh Yadav <shaileshyadav1416@gmail.com>
2022-12-29 16:49:55 +0530

    second commit

---
+++ print_ex2.py
@@ -0,0 +1,5 @@
+list_of_number = [1,2,3,4,5,6,7,8,9,10]
+
+# use for loop and print square of each number
+# ================output================
+# 1 4 9 16 25 36 49 64 81 100
--- print_exercise.py
+++ print_exercise.py
@@ -1,6 +1,7 @@
 print("Hello My name is Shailsh I live in boisar.")
 
 # modify code to return following output
+# use triple quotes """
 # ================output================
 # Hello
 # My name is Shailsh
