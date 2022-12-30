62f535881b98ddbf9bb9d3e3283cb34af9be5db7
Shailesh Yadav <shaileshyadav1416@gmail.com>
2022-12-30 17:16:23 +0530

    list ex3 commit

---
+++ list_ex3.py
@@ -0,0 +1,34 @@
+# below is a given list
+# do as the following on given list
+
+list_of_student =["aman","chaman","sohan","mohan","monu","sonu","rinku","pinku","aman","mahesh"]
+
+# find how many students are by name aman
+# code here
+
+
+# find how total student in list
+# code here
+
+
+# find how many name starts with m
+# code here
+
+
+# print all the students list as below
+# use for loop with range() and len() functiom
+# 1 aman
+# 2 chaman
+# 3 sohan
+# 4 mohan
+# 5 monu
+# 6 sonu
+# 7 rinku
+# 8 pinku
+# 9 aman
+# 10 mahesh
+
+# code here
+
+
+
