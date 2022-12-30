b370728434cb890ec6b512d758abd7305a01eb7e
Shailesh Yadav <shaileshyadav1416@gmail.com>
2022-12-30 17:58:33 +0530

    list ex4 commit

---
+++ list_ex4.py
@@ -0,0 +1,21 @@
+# Q. Understant the code 
+item_details = [
+    ["101","Mouse",200],
+    ["102","Keyboard",200],
+    ["103","Printer",12000],
+    ["104","Monitor",6000],
+    ["105","CPU",10000]
+]
+
+# ==============================
+# print the item details of Monitor from item_list
+# output
+# ===== ITEM DETAILS =====
+# Id      : 104
+# Name    : Monitor
+# Price   : 6000
+
+print("===== ITEM DETAILS =====")
+print("Id \t:",item_details[3][0])
+print("Name \t:",item_details[3][1])
+print("Price \t:",item_details[3][2])
