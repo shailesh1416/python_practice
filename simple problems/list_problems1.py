f822ee736e95648f68096e0d5eac10845fae8398
Shailesh Yadav <shaileshyadav1416@gmail.com>
2023-01-28 08:22:00 +0530

    simple list example

---
+++ simple problems/list_problems1.py
@@ -0,0 +1,28 @@
+stations = ['Dahanu','Vangao','Boisar','Palghar','Umroli','Kelve Road','Saphale','Vaitarna','Virar']
+
+
+# print total number of stations in list
+
+
+
+# print first statiom from list
+
+
+
+# print last statiom from list
+
+
+
+# take a station as input from user
+# and print the next station
+
+
+
+
+# take a station as input from user
+# and print the previous station
+
+
+
+
+
---
+++ simple problems/list_problems2.py
@@ -0,0 +1,8 @@
+stations = ['Dahanu','Vangao','Boisar','Palghar','Umroli','Kelve Road','Saphale','Vaitarna','Virar']
+
+# write a function which returns first statiom from list
+
+
+
+# write a function which returns last statiom from list
+
